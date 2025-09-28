eed0205476d45c8ded95ba065979fae713598301
wilkenio <wilkeniopereiraguitarra@gmail.com>
2025-09-28 16:35:04 -0300

    📦 NEW: inicio do menu da landingPage

--- src/app/shared/components/cliente-final/menu/menu.component.ts
+++ src/app/shared/components/cliente-final/menu/menu.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
-  imports: [],
+  imports: [CommonModule,RouterModule],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
