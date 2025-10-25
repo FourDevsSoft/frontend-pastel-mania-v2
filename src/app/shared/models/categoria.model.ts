f7f373124d26a40d4f22ff8af9eccb86529e6078
yuri <yurigx12345@gmail.com>
2025-10-25 14:59:37 -0300

    🐛 FIX: Melhorias na conexão do front com back

--- src/app/shared/models/categoria.model.ts
+++ src/app/shared/models/categoria.model.ts
@@ -1,15 +1,18 @@
+// categoria.model.ts
 export interface Categoria {
-    id?: number;
-    nome: string;
-    descricao: string;
-    dataCriacao?: string;
-    dataAtualizacao?: string;
-  }
-  
-  /** Estrutura esperada de resposta da API */
-  export interface CategoriasResponse {
-    data: {
-      categorias: Categoria[];
-    };
-  }
-  
+  id?: number;           // Para o frontend usar
+  id_categoria?: number; // Se o backend retorna assim
+  nome: string;
+  descricao: string;
+  ativo?: boolean;
+  dataCriacao?: string;
+  dataAtualizacao?: string;
+}
+
+
+/** Estrutura esperada de resposta da API */
+export interface CategoriasResponse {
+  data: {
+    categorias: Categoria[];
+  };
+}
